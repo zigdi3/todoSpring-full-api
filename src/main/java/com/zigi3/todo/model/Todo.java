200634ad31b098c2343cf2f79b4b25e99e1b9675
Diego Araujo <diegozigoto@gmail.com>
2021-02-19 23:07:35 -0300

    model todo created

---
+++ src/main/java/com/zigi3/todo/model/Todo.java
@@ -0,0 +1,42 @@
+package com.zigi3.todo.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Todo {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	private Long id;
+	
+	
+	@Column
+	private String description;
+	
+	@Column
+	private Boolean done;
+	
+	@Column
+	private LocalDateTime createdDate;
+	
+	@Column
+	private LocalDateTime doneDate;
+
+public static void main(String [] args)
+{ Todo todo = new Todo();
+	
+}
+
+}
